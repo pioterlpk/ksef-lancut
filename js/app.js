5dca3f4f03c1b7f9feeaa87d851976d543890bf8
pioterlpk <wilczeklpk@gmail.com>
2026-02-03 17:40:32 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-const s = JSON.parse(localStorage.getItem("session"));
-if (!s) location.href = "index.html";
+const session = JSON.parse(localStorage.getItem("session"));
+if(!session) location.href="index.html";
 
-user.innerText = s.login;
-unit.innerText = s.unit;
+document.getElementById("user").innerText = session.login;
+document.getElementById("unit").innerText = session.unit;
 
 function show(id){
     document.querySelectorAll(".section").forEach(x=>x.classList.add("hidden"));
@@ -16,43 +16,43 @@ function logout(){
 }
 
 function calculate(){
-    const qty = +p_qty.value;
-    const price = +p_price.value;
-    const vatP = +p_vat.value;
+    const qty = +document.getElementById("p_qty").value;
+    const price = +document.getElementById("p_price").value;
+    const vatP = +document.getElementById("p_vat").value;
 
     const net = qty * price;
-    const vat = net * vatP / 100;
+    const vat = net * vatP/100;
     const gross = net + vat;
 
-    netto.innerText = net.toFixed(2);
-    vat.innerText = vat.toFixed(2);
-    brutto.innerText = gross.toFixed(2);
+    document.getElementById("netto").innerText = net.toFixed(2);
+    document.getElementById("vat").innerText = vat.toFixed(2);
+    document.getElementById("brutto").innerText = gross.toFixed(2);
 }
 
 function addInvoice(){
     calculate();
     const inv = {
-        number: "FV/" + new Date().getFullYear() + "/" + Date.now(),
-        buyer: b_name.value,
-        gross: brutto.innerText,
+        number: "FV/"+new Date().getFullYear()+"/"+Date.now(),
+        buyer: document.getElementById("b_name").value,
+        gross: document.getElementById("brutto").innerText,
         date: new Date().toLocaleString()
     };
 
-    const list = JSON.parse(localStorage.getItem("invoices"));
-    list.push(inv);
-    localStorage.setItem("invoices", JSON.stringify(list));
+    const invoices = JSON.parse(localStorage.getItem("invoices"));
+    invoices.push(inv);
+    localStorage.setItem("invoices", JSON.stringify(invoices));
     alert("Faktura zapisana");
 }
 
 function loadInvoices(){
-    invoiceList.innerHTML="";
-    JSON.parse(localStorage.getItem("invoices")).forEach(i=>{
-        invoiceList.innerHTML += `
-        <tr>
-            <td>${i.number}</td>
-            <td>${i.buyer}</td>
-            <td>${i.gross} PLN</td>
-            <td>${i.date}</td>
+    const invoiceList = document.getElementById("invoiceList");
+    invoiceList.innerHTML = "";
+    JSON.parse(localStorage.getItem("invoices")).forEach(inv=>{
+        invoiceList.innerHTML += `<tr>
+            <td>${inv.number}</td>
+            <td>${inv.buyer}</td>
+            <td>${inv.gross} PLN</td>
+            <td>${inv.date}</td>
         </tr>`;
     });
 }
