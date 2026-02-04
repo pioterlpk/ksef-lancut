d474a874ee52c17eca987d95599a0b623e7cfb5d
pioterlpk <wilczeklpk@gmail.com>
2026-02-04 21:48:13 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,10 +1,6 @@
 if(!localStorage.getItem("users")){
     localStorage.setItem("users", JSON.stringify([
-        {login:"admin",password:"admin123",unit:"KPP",role:"admin"},
-        {login:"psp01",password:"psp123",unit:"PSP",role:"user"},
-        {login:"osp01",password:"osp123",unit:"OSP",role:"user"},
-        {login:"sd01",password:"sd123",unit:"Służba Drogowa",role:"user"},
-        {login:"med01",password:"med123",unit:"RSPR",role:"user"}
+        {login:"admin",password:"admin",unit:"KSeF",role:"admin"}
     ]));
 }
 
